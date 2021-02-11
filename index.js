dc54702247db5e45685fc18d8aecb5c7e91539f5
Discovery <69199746+Daring-Games@users.noreply.github.com>
2021-02-12 00:07:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const modal = document.getElementById( "newModal" );
+
+const btn = document.getElementById( "btn" );
+
+const span = document.getElementsByClassName( "close" )[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function( event ) {
+    if ( event.target === modal ) {
+        modal.style.display = "none";
+    }
+}
